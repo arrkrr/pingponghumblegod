892b3570be4e2aae4dc0ba780fa3c5f685bfbb73
arakar <113598638+arrkrr@users.noreply.github.com>
2023-04-15 14:04:29 +0700

    with UI

--- MyStack.java
+++ MyStack.java
@@ -5,34 +5,51 @@ import java.util.Scanner;
 public class MyStack {
 	int stack[];
 	Scanner s;
-	int top;
+	int top = -1; 			//stack is empty
 	int n;
-	MyStack(){
+	public static void main(String[]args) {
+		int x;
+		MyStack ms = new MyStack();
+		do {
+		ms.s = new Scanner(System.in);
+		System.out.println("Choose your operation: \n1.Push\n2.Pop\n3.Display\n4.Check if stack is empty\n5.Check is stack is full\n6.Number of Elements in the stack\n7.Number of empty locations in the stack\n8.Exit\nInput your choice: ");
+		x = ms.s.nextInt();
+			if(x==1) {
+				ms.size();
+				ms.push();
+			}else if(x==2) {
+				ms.pop();
+			}else if(x==3) {
+				ms.display();
+			}else if(x==4){
+				ms.isEmpty();
+			}else if(x==5){
+				ms.isFull();
+			}else if(x==6){
+				ms.numOfElements();
+			}else if(x==7) {
+				ms.countEmpty();
+			}else {
+				System.out.println("Goodbye");
+			}
+		}while(x!=8);
+	}
+	void size(){
 		s = new Scanner(System.in);
 		System.out.println("What is the size of your array?");
 		n = s.nextInt();	
 		stack = new int[n];		
-		top = -1;	//stack is empty
-	}
-	public static void main(String[]args) {
-		MyStack ms = new MyStack();
-		ms.push();
-		ms.push();
-		ms.push();
-		ms.push();
-		ms.push();
-		ms.display();
-		ms.pop();
-		ms.pop();
-		ms.display();
 	}
 	void push() {
+		for (int i=0; i<n; i++) {
 		if(top<(n-1)){		/*stack is not full*/
 			top++;
 			System.out.println("Enter an integer to push");
 			stack[top] = s.nextInt();
+			//System.out.println(stack[top]);
 		}else {
 			System.out.println("Cannot push, stack is full");
+		}
 		}
 	}
 	void pop() {
@@ -51,5 +68,24 @@ public class MyStack {
 		}else{
 			System.out.println("Stack is empty");
 		}
+	}
+	void isEmpty() {
+		if(top==-1) {
+			System.out.println("Stack is empty");
+		}else {
+			System.out.println("Stack is not empty");
+		}
+	}
+	void isFull() {
+		if(top==n-1)
+			System.out.println("Stack is full");
+		else
+			System.out.println("Stack is not full");
+	}
+	void numOfElements() {
+		System.out.println("There are"+(top+1)+"elements");
+	}
+	void countEmpty() {
+		System.out.println("There are "+(n-1-top)+"empty slots");
 	}
 }
